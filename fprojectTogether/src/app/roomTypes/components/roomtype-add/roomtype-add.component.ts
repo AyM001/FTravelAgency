17fac1664ff5951cf4b87816e68f60962be9eee4
taviJava <tavi.zorila@gmail.com>
2020-11-05 19:06:08 +0200

    last for today frontend

--- fprojectTogether/src/app/roomTypes/components/roomtype-add/roomtype-add.component.ts
+++ fprojectTogether/src/app/roomTypes/components/roomtype-add/roomtype-add.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {RoomType} from '../../model/room-type';
 import {ActivatedRoute, Router} from '@angular/router';
 import {RoomTypeService} from '../../services/room-type.service';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-roomtype-add',
@@ -10,12 +11,18 @@ import {RoomTypeService} from '../../services/room-type.service';
 })
 export class RoomtypeAddComponent implements OnInit {
 roomType: RoomType = new RoomType();
+words: string[] = ['no' , 'yes'];
+balcony = 'no';
+myGroup: FormGroup;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
               private roomTypeService: RoomTypeService) { }
 
   ngOnInit(): void {
+    this.myGroup = new FormGroup({
+      name: new FormControl(this.balcony)
+    });
   }
 // tslint:disable-next-line:typedef
   getRoomTypeList() {
@@ -23,6 +30,13 @@ roomType: RoomType = new RoomType();
   }
   // tslint:disable-next-line:typedef
   onSubmit(){
+    this.roomType.hasbalcony = this.hasBalcony();
     this.roomTypeService.save(this.roomType).subscribe(result => this.getRoomTypeList());
+  }
+  hasBalcony(): boolean{
+    if (this.balcony === 'yes'){
+      return true;
+    }
+    return false;
   }
 }
--- fprojectTogether/src/app/roomTypes/model/room-type.ts
+++ fprojectTogether/src/app/roomTypes/model/room-type.ts
@@ -3,6 +3,8 @@ import {Room} from '../../rooms/model/room';
 export class RoomType {
   id: number;
   name: string;
+  description: string;
   places: number;
+  hasbalcony: boolean;
   roomModelList: Room[];
 }
